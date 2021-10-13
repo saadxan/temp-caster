05aadc6255a6e115d992f2e709627af953a899f2
Saad Khan <skhan48@toromail.csudh.edu>
2021-10-13 12:14:38 -0700

    Deploy GH Pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import WeatherCard from './WeatherCard.js'
-
+//
 
 class App extends React.Component {
 
